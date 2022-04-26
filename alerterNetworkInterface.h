824adba4576e416139cea214afd7cc537327aa92
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-04-26 12:48:37 +0530

    Update alerterNetworkInterface.h

--- alerterNetworkInterface.h
+++ alerterNetworkInterface.h
@@ -5,6 +5,7 @@ int networkAlert(float celcius)
 {
     std::cout << "Integration Environment \n";
     /* As the Real Network Communication Interface is not available, assuming the celsius value is less than threshold*/
+    (void)celcius;
     return SUCCESS;
 }
 
