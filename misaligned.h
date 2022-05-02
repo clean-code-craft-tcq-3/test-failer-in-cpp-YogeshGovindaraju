e1345dc3ec00dfc749c50b0d573723d3034b0e37
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-05-02 14:56:16 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -2,7 +2,6 @@
 #define MISALIGNED
 
 #include <iostream>
-#include <assert.h>
 #include <vector>
 #include <string>
 #include <iomanip>
@@ -26,14 +25,14 @@ int printColorMap(ColorPair &colorPair, ColorPair &colorPairAlignment)
     {
         for(j = 0; j < 5; j++)
         {
-            pairNumber = std::to_string(i * 5 + j);
-            pairNumberBuffer <<  pairNumber ;
-            majorColorBuffer <<  majorColor[i] ;
-            minorColorBuffer <<  minorColor[i] ;
+            pairNumber = std::to_string(i * 5 + j + 1);
+            pairNumberBuffer << std::setw(10) << pairNumber ;
+            majorColorBuffer << std::setw(10) << majorColor[i] ;
+            minorColorBuffer << std::setw(10) << minorColor[j] ;
             std::cout << pairNumberBuffer.str() << " | " << majorColorBuffer.str() << " | " << minorColorBuffer.str() << "\n";
             colorPair.number.push_back(pairNumber);
             colorPair.majorColorList.push_back(majorColor[i]);
-            colorPair.minorColorList.push_back(minorColor[i]);
+            colorPair.minorColorList.push_back(minorColor[j]);
             colorPairAlignment.number.push_back(pairNumberBuffer.str());
             colorPairAlignment.majorColorList.push_back(majorColorBuffer.str());
             colorPairAlignment.minorColorList.push_back(minorColorBuffer.str());
