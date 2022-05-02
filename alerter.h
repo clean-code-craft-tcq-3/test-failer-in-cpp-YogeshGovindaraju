daad82387715026e9ee87365e4a6ce4a2d60840c
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-05-02 15:00:25 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,25 +1,30 @@
 #ifndef ALERTER
 #define ALERTER
 
-#include "alerterInfo.h"
+#define ZERO 0
+#define ONE 1
+#define SUCCESS 200
+#define FAILED 500
+#define CELCIUSTHRESHOLD 150
+
+#include <iostream>
+#include <assert.h>
 #include "alerterStub.h"
 #include "alerterNetworkInterface.h"
 
-void alertInCelcius(float farenheit)
+float convertFarenheitToCelsius(float farenheit)
 {
-    float celcius = FARENHEITTOCELCIUSFORMULA(farenheit);
+    return (farenheit - 32) * 5 / 9 ;
+}
+
+void alertInCelcius(float farenheit, int &alertFailureCount, int (*networkAlert)(float))
+{
+    float celcius = convertFarenheitToCelsius(farenheit);
     int returnCode = SUCCESS;
-    if(getCodeEnvironment() == TEST)
-    {
-        returnCode = networkAlertStub(celcius);
-    }
-    else if (getCodeEnvironment() == INTEGRATION)
-    {
-        returnCode = networkAlert(celcius);
-    }
+    returnCode = networkAlert(celcius);
     if (returnCode != SUCCESS)
     {
-        alertFailureCount += ZERO;
+        alertFailureCount += ONE;
     }
 }
 
